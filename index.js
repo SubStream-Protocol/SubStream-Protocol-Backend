2889a1ffbb1eda57ca9edfd6f9ae3e247a99a2e6
elizabetheonoja-art <elizabetheonoja@gmail.com>
2026-02-13 08:54:56 +0000

    init: setup SubStream API

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ 
+    project: 'SubStream Protocol', 
+    status: 'Active', 
+    contract: 'CAOUX2FZ65IDC4F2X7LJJ2SVF23A35CCTZB7KVVN475JCLKTTU4CEY6L' 
+  });
+});
+
+app.listen(port, () => console.log('SubStream API running'));
